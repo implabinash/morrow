8b1e65a10faa89702753696729ff2e1c3f55efbe
Abinash Sahoo <implabinash@gmail.com>
2025-08-13 21:33:36 +0530

    style: design header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Plus } from '@lucide/svelte';
 </script>
 
 <svelte:head>
@@ -6,4 +7,23 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>morrow</section>
+<header
+	class="flex items-center justify-between border-b border-b-neutral-border bg-[#fdf9fa] px-12 py-4"
+>
+	<div class="flex gap-2">
+		<img src="/images/logo.svg" alt="Morrow" class="h-8 w-8" />
+
+		<a href="/" class="text-heading-1 font-semibold">Morrow</a>
+	</div>
+
+	<nav>
+		<a
+			href="/"
+			class="flex items-center gap-2 rounded-md bg-brand-600 py-2 pr-4 pl-3 text-body-bold text-white hover:bg-brand-500 active:bg-brand-600"
+		>
+			<Plus size="20px" /> New Hint</a
+		>
+	</nav>
+</header>
+
+<section></section>
