d5e225e200f89a452409c54706cf571cf77c363c
Abinash Sahoo <implabinash@gmail.com>
2025-08-20 18:07:15 +0530

    db: create table schema for hints

--- src/lib/server/db/schema/hint.ts
+++ src/lib/server/db/schema/hint.ts
@@ -1,3 +1,44 @@
-import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { randomUUID } from "crypto";
+import { sql } from "drizzle-orm";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const hintsTable = sqliteTable("hint", {});
+import { categories } from "$lib/utils/categories";
+import { difficulties } from "$lib/utils/difficulty";
+
+export const hintsTable = sqliteTable("hint", {
+	id: text("id")
+		.primaryKey()
+		.notNull()
+		.unique()
+		.$defaultFn(() => randomUUID()),
+
+	title: text("title").notNull(),
+	description: text("description").notNull(),
+
+	category: text("category", {
+		enum: categories.map((c) => c.id) as [
+			(typeof categories)[number]["id"],
+			...(typeof categories)[number]["id"][]
+		]
+	}).notNull(),
+
+	difficulty: text("difficulty", {
+		enum: difficulties.map((d) => d.name) as [
+			(typeof difficulties)[number]["name"],
+			...(typeof difficulties)[number]["name"][]
+		]
+	}),
+
+	resources: text("resources").notNull(),
+
+	avatar: text("avatar"),
+	name: text("name").notNull(),
+	twitter: text("twitter"),
+
+	createdAt: text("created_at")
+		.notNull()
+		.default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: text("updated_at")
+		.notNull()
+		.default(sql`CURRENT_TIMESTAMP`)
+});
