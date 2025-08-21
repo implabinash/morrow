6b4e17fa0388a72b7bdd118b575cb632d59aff87
Abinash Sahoo <implabinash@gmail.com>
2025-08-21 06:50:13 +0530

    db: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "drizzle-kit";
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 export default defineConfig({
-	schema: "./src/lib/server/db/schema.ts",
+	out: "./drizzle",
+	schema: "./src/lib/server/db/schema",
 	dialect: "turso",
 	dbCredentials: {
-		authToken: process.env.DATABASE_AUTH_TOKEN,
+		// authToken: process.env.DATABASE_AUTH_TOKEN,
 		url: process.env.DATABASE_URL
 	},
 	verbose: true,
