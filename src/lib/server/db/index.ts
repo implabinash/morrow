6e82987a87323502ce5d7f31bf196a4b5b574d85
Abinash Sahoo <implabinash@gmail.com>
2025-08-24 17:03:35 +0530

    fix: make the env variable static

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,14 +1,14 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import { env } from "$env/dynamic/private";
+import { DATABASE_URL, DATABASE_AUTH_TOKEN } from "$env/static/private";
 import { hintsTable } from "./schema/hint";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-if (!env.DATABASE_AUTH_TOKEN) throw new Error("DATABASE_AUTH_TOKEN is not set");
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!DATABASE_AUTH_TOKEN) throw new Error("DATABASE_AUTH_TOKEN is not set");
 
 const client = createClient({
-	url: env.DATABASE_URL,
-	authToken: env.DATABASE_AUTH_TOKEN
+	url: DATABASE_URL,
+	authToken: DATABASE_AUTH_TOKEN
 });
 
 export const db = drizzle({ client, schema: { hintsTable } });
