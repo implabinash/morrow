f6435dd9ef012e2ef83241796c3e4e0328158e28
Abinash Sahoo <implabinash@gmail.com>
2025-08-24 16:58:56 +0530

    feat: add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
 	import "../app.css";
 
 	import type { LayoutProps } from "./$types";
@@ -6,6 +8,8 @@
 	import Header from "$lib/components/Header.svelte";
 
 	let { children }: LayoutProps = $props();
+
+	injectAnalytics();
 </script>
 
 <div class="app">
